1595517f23950bb4f0e25ccd06d4fe425b5f2459
MahdiMurshed <123mahdi12345@gmail.com>
2023-03-12 23:59:10 +0600

    feat: remove font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import ErrorBoundary from "@components/ErrorBoundary";
 import AdminLayout from "@components/admin/layout";
+import ErrorBoundary from "@components/ErrorBoundary";
 import { fetchMaterials } from "@hooks/materials";
 import { MantineProvider } from "@mantine/core";
-import { Inter } from "@next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Session } from "next-auth";
@@ -25,11 +24,7 @@ export const queryClient = new QueryClient({
   // },
 });
 
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "800"],
-  variable: "--inter",
-});
+
 
 type AppProps = {
   Component: Props["Component"];
@@ -74,7 +69,7 @@ export default function App({ Component, pageProps, session }: AppProps) {
             <Head>
               <meta name="viewport" content="viewport-fit=cover" />
             </Head>
-            <main className={inter.className}>
+            <main>
               {isAdminRoute ? (
                 <AdminLayout>
                   <Component {...pageProps} />
